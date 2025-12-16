0740f640b5f98c341d5b5b7d753de794be8680ac
yasminehdg <haddagyasmine9@gmail.com>
2025-12-16 05:13:12 +0100

    Fix wsgi settings module name

--- comautis/wsgi.py
+++ comautis/wsgi.py
@@ -1,5 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ComAutiste.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comautis.settings')
+
 application = get_wsgi_application()
